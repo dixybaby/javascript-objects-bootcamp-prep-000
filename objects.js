aa7907e0940476a5a0ab8e44433843d4a72efa93
Barbara Dixon <babsnwosisi@yahoo.com>
2017-09-28 11:15:38 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,3 +2,8 @@ var playlist = {
   'Willis': 'Die Hard',
   'Smith' : 'Men in Black'
 }
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artist] = song
+  return playlist
+}
